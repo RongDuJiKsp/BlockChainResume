4fdab3b603c4a71b9cdb0d484d5fe71274368225
宇麒 周 <rdjksp@qq.com>
2023-07-20 21:00:37 +0800

    genshin

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -9,35 +9,37 @@ export default function Home(props) {
     return (
 <div>
     <Content style={{height:"700px"}} className={"HomeContent"}>
-        <Row>
-            <Col span={8}>
-                <Card hoverable={true} title="发起一个请求" extra={
-                    <button className={"btn green small"}>Jump</button>
-                } style={{width: 300, marginLeft: "5px"}}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
-            </Col>
-            <Col span={8}>
-                <Card hoverable={true} title="Default size card" extra={
-                    <button className={"btn green small"}></button>
-                } style={{width: 300, marginLeft: "5px"}}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
-            </Col>
-            <Col span={8}>
-                <Card hoverable={true} title="Default size card" extra={
-                    <button className={"btn green small"}></button>
-                } style={{width: 300, marginLeft: "5px"}}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
-            </Col>
-        </Row>
+       <div id={"HomeCardList"}>
+           <Row>
+               <Col span={8}>
+                   <Card hoverable={true} title="发起一个请求" extra={
+                       <button className={"btn green small"}>Jump</button>
+                   } style={{width: 300, marginLeft: "5px"}}>
+                       <p>Card content</p>
+                       <p>Card content</p>
+                       <p>Card content</p>
+                   </Card>
+               </Col>
+               <Col span={8}>
+                   <Card hoverable={true} title="Default size card" extra={
+                       <button className={"btn green small"}></button>
+                   } style={{width: 300, marginLeft: "5px"}}>
+                       <p>Card content</p>
+                       <p>Card content</p>
+                       <p>Card content</p>
+                   </Card>
+               </Col>
+               <Col span={8}>
+                   <Card hoverable={true} title="Default size card" extra={
+                       <button className={"btn green small"}></button>
+                   } style={{width: 300, marginLeft: "5px"}}>
+                       <p>Card content</p>
+                       <p>Card content</p>
+                       <p>Card content</p>
+                   </Card>
+               </Col>
+           </Row>
+       </div>
     </Content>
 </div>
     )
