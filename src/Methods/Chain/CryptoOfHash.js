ea2ce196d1640ffa5808b5cc430a627a6695e89a
宇麒 周 <rdjksp@qq.com>
2023-08-05 12:17:22 +0800

    小更新

--- src/Methods/Chain/CryptoOfHash.js
+++ src/Methods/Chain/CryptoOfHash.js
@@ -1,18 +1,10 @@
 const {SM4} = require('gm-crypto')
 const crypto = require("crypto");
-const prolongingStrTo32 = (key) => {
-    let res = "" + key;
-    for (let i = res.length; i < 32; i++) {
-        res += "1";
-    }
-    return res;
-}
 const CryptoOfHash = {
-    prolongingStrTo32: prolongingStrTo32(),
     encryptedData: (originalData//ipfs文件hash
         , key//s
     ) => {
-        return SM4.encrypt(originalData, prolongingStrTo32(key), {
+        return SM4.encrypt(originalData, key, {
             inputEncoding: 'utf8',
             outputEncoding: 'base64'
         })
@@ -20,7 +12,7 @@ const CryptoOfHash = {
     decryptedData: (encryptedData//加密后的文件hash
         , key//s
     ) => {
-        return SM4.decrypt(encryptedData, prolongingStrTo32(key), {
+        return SM4.decrypt(encryptedData, key, {
             inputEncoding: 'base64',
             outputEncoding: 'utf8'
         })
