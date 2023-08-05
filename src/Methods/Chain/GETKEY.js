670d1376425841d066d51d6ca378172aa57e248a
宇麒 周 <rdjksp@qq.com>
2023-08-05 13:01:57 +0800

    校验CA秘钥

---
+++ src/Methods/Chain/GETKEY.js
@@ -0,0 +1,321 @@
+const GETKEY = function (walletAddressOfCompany) {
+    return new Promise((resolve, reject) => {
+        let Web3 = require("web3");
+        let web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+        let abi = [
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "company",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "ownuser",
+                        "type": "address"
+                    }
+                ],
+                "name": "givenpower",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "internalType": "string",
+                        "name": "resultfedback",
+                        "type": "string"
+                    }
+                ],
+                "name": "ResultX",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "internalType": "string",
+                        "name": "fedback",
+                        "type": "string"
+                    }
+                ],
+                "name": "givenpowerresult",
+                "type": "event"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "ca",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "userid",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "enhash",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "useradd",
+                        "type": "address"
+                    }
+                ],
+                "name": "signPASS",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "internalType": "string",
+                        "name": "fedback",
+                        "type": "string"
+                    },
+                    {
+                        "indexed": false,
+                        "internalType": "uint256",
+                        "name": "key",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "tryget",
+                "type": "event"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "company",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "ownuser",
+                        "type": "address"
+                    }
+                ],
+                "name": "trygetkey",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "ps",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "ms",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "xs",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "userid",
+                        "type": "string"
+                    }
+                ],
+                "name": "upload_sonkey",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "internalType": "string",
+                        "name": "fedback",
+                        "type": "string"
+                    }
+                ],
+                "name": "uploadkeysfedback",
+                "type": "event"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "string",
+                        "name": "enhash",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "userid",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "useradd",
+                        "type": "address"
+                    }
+                ],
+                "name": "UploadResume",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "",
+                        "type": "address"
+                    }
+                ],
+                "name": "addtoid",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "",
+                        "type": "address"
+                    }
+                ],
+                "name": "codepower",
+                "outputs": [
+                    {
+                        "internalType": "bool",
+                        "name": "",
+                        "type": "bool"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256[]",
+                        "name": "m_t",
+                        "type": "uint256[]"
+                    }
+                ],
+                "name": "divresult",
+                "outputs": [
+                    {
+                        "internalType": "uint256[]",
+                        "name": "",
+                        "type": "uint256[]"
+                    }
+                ],
+                "stateMutability": "pure",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "a",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "p",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "invmod",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "pure",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "name": "number",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "p",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256[]",
+                        "name": "m",
+                        "type": "uint256[]"
+                    },
+                    {
+                        "internalType": "uint256[]",
+                        "name": "a",
+                        "type": "uint256[]"
+                    }
+                ],
+                "name": "verify_recover",
+                "outputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "pure",
+                "type": "function"
+            }
+        ];
+        let contractAddress = '0xF97CA4a5187bBd66767d5bFef32D29812b17D9be';
+        let myContract = new web3.eth.Contract(abi, contractAddress);
+        myContract.methods.givenpower(walletAddressOfCompany)
+            .send({from: walletAddressOfCompany, gas: 1000000})
+            .on('receipt', function (receipt) {
+                myContract.getPastEvents('tryget', {
+                    toBlock: 'latest'
+                }, function (error, events) {
+                    resolve(events[0].returnValues);
+                }).then()
+            })
+            .catch((error) => {
+                reject(error); // 处理发送交易错误
+            });
+    });
+
+}
+
+export default GETKEY;
